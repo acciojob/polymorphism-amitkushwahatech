8d2dcaaded0c36e8e032f1146d08a566f6a0f6eb
amitkushwahatech <amitkushwahatech@gmail.com>
2023-01-12 23:08:33 +0530

    Solution 1 after changes 7

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,42 +7,23 @@ public class Main {
         p.product(5,10);
         p.product(5,10,15);
         p.product(10.21,5.2);
+        System.out.println(p.product(2, 3) + " " + p.product(2, 3, 4) + " " + p.product(2.0, 3.0));
     }
 
   public static  class   Product{
-        public int product(int x, int y) {
+        public static int product(int x, int y) {
             System.out.println("Method 1");
             return (x+y);
         }
-        public int product(int x, int y, int z) {
+        public static int product(int x, int y, int z) {
             System.out.println("Method 2");
             return (x+y+z);
         }
-        public double product(double x, double y) {
+        public static double product(double x, double y) {
             System.out.println("Method 3");
             return (x+y);
         }
     }
 
-  /*  public static void main(String[] args) {
-        Product p = new Product();
-        System.out.println(p.product(2, 3) + " " + p.product(2, 3, 4) + " " + p.product(2.0, 3.0));
-    }
 
-
-    public static class Product{
-        public static int product(int x, int y) {
-            return x*y;
-        }
-
-        public static int product(int x, int y, int z) {
-            return x*y*z;
-        }
-
-        public static double product(double x, double y) {
-            return x*y;
-        }
-    }
-
-   */
 }
